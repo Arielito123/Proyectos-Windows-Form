ec3bfc2168f3a949fd62adbcbed205c4eb21d1e5
Arielito123 <104540300+Arielito123@users.noreply.github.com>
2023-11-03 17:46:22 -0300

    Tateti c#

---
+++ TaTeTi/TaTeTi/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TaTeTi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTurno = new System.Windows.Forms.Label();
+            this.lblMovimientos = new System.Windows.Forms.Label();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTurno
+            // 
+            this.lblTurno.AutoSize = true;
+            this.lblTurno.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurno.Location = new System.Drawing.Point(856, 37);
+            this.lblTurno.Name = "lblTurno";
+            this.lblTurno.Size = new System.Drawing.Size(0, 29);
+            this.lblTurno.TabIndex = 0;
+            // 
+            // lblMovimientos
+            // 
+            this.lblMovimientos.AutoSize = true;
+            this.lblMovimientos.Location = new System.Drawing.Point(804, 133);
+            this.lblMovimientos.Name = "lblMovimientos";
+            this.lblMovimientos.Size = new System.Drawing.Size(0, 20);
+            this.lblMovimientos.TabIndex = 1;
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(1035, 401);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(100, 36);
+            this.btnReiniciar.TabIndex = 2;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.AccessibleRole = System.Windows.Forms.AccessibleRole.Sound;
+            this.btnSalir.Location = new System.Drawing.Point(1035, 474);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(100, 36);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Sound;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1235, 729);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.lblMovimientos);
+            this.Controls.Add(this.lblTurno);
+            this.Name = "Form1";
+            this.Text = "Salir";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTurno;
+        private System.Windows.Forms.Label lblMovimientos;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
